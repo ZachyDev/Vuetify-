54a14880ec118cba64a6813ffb304343f717278c
ZachyDev <mosetizachary001@gmail.com>
2020-11-05 16:44:57 +0300

    Vuetify UI componennts: avatars

--- shopi/src/App.vue
+++ shopi/src/App.vue
@@ -12,6 +12,11 @@
         <v-alert color="red lighten-2" border="top"></v-alert>
         <v-alert border="top" type="success" colored-border color="deep-purple accent-4" elevation="2"></v-alert>
 
+        <!-- avatars -->
+        <v-avatar color="indigo" size="50">
+            <span class="white--text">20</span>
+        </v-avatar>
+
     </v-main>
 </v-app>
 </template>
